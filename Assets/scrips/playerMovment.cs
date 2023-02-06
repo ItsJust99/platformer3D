67d1a968ca329ef532ad0a8f3f0985a23af58d9d
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-02-06 12:31:26 +0100

    update drag

--- Assets/scrips/playerMovment.cs
+++ Assets/scrips/playerMovment.cs
@@ -7,6 +7,13 @@ public class playerMovment : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
     public Transform orientation;
 
     float horizontalInput;
@@ -26,7 +33,16 @@ public class playerMovment : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
         MyInput();
+
+        //handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
     private void FixedUpdate()
