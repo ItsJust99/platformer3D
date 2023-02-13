5641581639ec0147abd2c342059a55da704a549b
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-02-13 12:31:33 +0100

    add stats

---
+++ Assets/scrips/Stats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScritableObjects/Stats")]
+public class Stats : ScriptableObject
+{
+    public float HP;
+    public float moveSpeed;
+    public float jumpHeigt;
+}
---
+++ Assets/scrips/shootAbility.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+[CreateAssetMenu(menuName = "ScritableObjects/Ability/ShootAbility")]
+public class shootAbility : Ability
+{
+    [SerializeField] private GameObject _bullet;
+    public override void Use(playerMovment player)
+    {
+        Instantiate(_bullet, player.transform.position, player.transform.rotation);
+    }
+}
+*/
