3956e901976bfead21cc21e275160a346defe74b
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-03-04 17:48:41 +0100

    muis

--- Assets/scrips/playerMovment.cs
+++ Assets/scrips/playerMovment.cs
@@ -6,6 +6,8 @@ public class PlayerMovment : MonoBehaviour
 {
     [SerializeField] private Transform _eyes;
     [SerializeField] private float _sensitivity;
+    [SerializeField] private float _camLimitMin;
+    [SerializeField] private float _camLimitMax;
     private float _camAngle = 0.0f;
 
     private void Start()
@@ -15,8 +17,19 @@ public class PlayerMovment : MonoBehaviour
     }
     private void Update()
     {
+        RotateEyes();
+        RotateBody();
+    }
+    private void RotateEyes()
+    {
         float yMouse = Input.GetAxis("Mouse Y") * _sensitivity * Time.deltaTime;
         _camAngle -= yMouse;
+        _camAngle = Mathf.Clamp(_camAngle, _camLimitMin, _camLimitMax);
         _eyes.localRotation = Quaternion.Euler(_camAngle, 0, 0);
     }
-}
+    private void RotateBody()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * xMouse);
+    }
+}   
