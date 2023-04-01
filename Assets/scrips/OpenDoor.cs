1956af4d8e8d2bb08b655c2767a40d3e22a787ec
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-04-01 16:58:10 +0200

    update map
    deuren gaan open level inpreciepe klaar nu de wapens + enemeys enz maken

---
+++ Assets/scrips/OpenDoor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Destroy(GameObject.FindWithTag("deurOpenen"));
+    }
+}
+
