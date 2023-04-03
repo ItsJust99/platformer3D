36ca7542f27d9c31363041f65395332017976fca
ItsJust99 <sa.van.der.heijden@sintlucasedu.nl>
2023-04-03 09:43:50 +0200

    start scherm

---
+++ Assets/scrips/Gun.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private KeyCode _fireKey;
+
+    void Update()
+    {
+        if (Input.GetKey(_fireKey))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
+    }
+}
